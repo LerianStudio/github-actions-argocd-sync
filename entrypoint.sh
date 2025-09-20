3fa9f43dfe11a4af64a0820adff382cae6af48ec
Gabriel Ferreira <ferr3ira.gabriel@gmail.com>
2025-09-20 16:01:11 -0300

    chore: set grpc web parameter in argocd cli commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,14 @@ ENV_PREFIX=$4
 APP_TO_SYNC="$APP_NAME-$ENV_PREFIX"
 
 echo -e "\033[0;36m ======> APP $APP_TO_SYNC \033[0m\n"
-argocd app get "$APP_TO_SYNC" --server "$ARGO_CD_URL" --auth-token "$ARGO_CD_TOKEN"
+argocd app get "$APP_TO_SYNC" --server "$ARGO_CD_URL" --auth-token "$ARGO_CD_TOKEN" --grpc-web
 
 sleep 5
 
 echo -e "\033[0;36m======> SYNC $APP_TO_SYNC \033[0m\n"
-argocd app sync "$APP_TO_SYNC" --server "$ARGO_CD_URL" --auth-token "$ARGO_CD_TOKEN"
+argocd app sync "$APP_TO_SYNC" --server "$ARGO_CD_URL" --auth-token "$ARGO_CD_TOKEN" --grpc-web
 
 echo -e "\033[0;36m======> WAIT SYNC $APP_TO_SYNC \033[0m\n"
-argocd app wait "$APP_TO_SYNC" --server "$ARGO_CD_URL" --auth-token "$ARGO_CD_TOKEN"
+argocd app wait "$APP_TO_SYNC" --server "$ARGO_CD_URL" --auth-token "$ARGO_CD_TOKEN" --grpc-web
 
 echo -e "\033[0;36m======> DONE \033[0m\n"
